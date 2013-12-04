f7f3698c102d5686d7633e6ae76fa7155ca7c3fd
Mohammad Sadeghi <sadeghi85@hotmail.com>
2013-12-04 15:27:37 +0330

    .

---
+++ app/config/pagoda/site.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+
+	'name' => 'markdown-wiki',
+	'domain' => 'markdown-wiki.gopagoda.com',
+
+	
+	'title' => 'Markdown Wiki',
+	'copyright' => '&copy; [Sadeghi85]@[hotmail.com] 2013',
+
+);
