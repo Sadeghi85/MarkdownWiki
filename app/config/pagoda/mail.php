49522e9dfd7ea3679a8f4ff91b4bc82e284711f7
Mohammad Sadeghi <sadeghi85@hotmail.com>
2013-12-08 21:30:54 +0330

    .

--- app/config/pagoda/mail.php
+++ app/config/pagoda/mail.php
@@ -106,7 +106,7 @@ return array(
 	|
 	*/
 
-	'sendmail' => '/usr/bin/pagoda-mail --app 52a47b16a5ee7c9043000891',
+	'sendmail' => '/usr/bin/pagoda-mail --app 52a47b16a5ee7c9043000891 -bs',
 
 	/*
 	|--------------------------------------------------------------------------
